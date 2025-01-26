3d7519120b4de15cc8334e82fa2dbdb45c9a672b
Rafale470 <ad.4.sourbets@gmail.com>
2025-01-26 15:51:31 +0100

    Update constantes.py
    ça servait à rien

--- other genetic/constantes.py
+++ other genetic/constantes.py
@@ -1,35 +1,6 @@
 import numpy as np
 import operator
 from fonctions import *
-def div_with0(a,b):
-    """Safely performs division, avoiding division by zero.
-    
-    Parameters:
-        a (float): The numerator.
-        b (float): The denominator.
-        
-    Returns:
-        float: The result of the division, or 0 if the denominator is 0."""
-    return operator.truediv(a, b) if b!=0 else 1
-
-def pow_with0(a,b):
-    """Safely performs the power operation, handling edge cases with zero and negatives.
-    
-    Parameters:
-        a (float): The base.
-        b (float): The exponent.
-        
-    Returns:
-        float: The result of the power operation, or 0 for invalid cases."""
-    """ Managing complex number too"""
-    if a < 0 and not(float(b).is_integer()):
-        return 0.0
-    res = operator.pow(a, b) if not(isinstance(b, complex)) and (b >=0 or a!=0) else 0.0
-
-    if (isinstance(res, complex)):
-        print("Alerte !")
-    
-    return res
 
 class GlobalConstant :
     def __init__(self, content):
