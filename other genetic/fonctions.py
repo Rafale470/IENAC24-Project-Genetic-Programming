c621993212b4e048ceff3bf1483c2c0dbc628f03
Rafale470 <ad.4.sourbets@gmail.com>
2025-01-26 21:41:52 +0100

    Delete other genetic directory

--- other genetic/fonctions.py
+++
@@ -1,7 +0,0 @@
-class fonction:
-    def __init__(self,noun,symbol,type):
-        self.noun = noun
-        self.symbol = symbol
-        self.type = type
-    def __repr__(self):
-        return f" {self.noun, self.symbol, self.type}"
