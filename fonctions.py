75fcc1bd0aba09aad258f666a52a1a40bc8ad4d9
Rafale470 <ad.4.sourbets@gmail.com>
2025-01-26 21:42:53 +0100

    Delete fonctions.py

--- fonctions.py
+++
@@ -1,8 +0,0 @@
-class fonction:
-    def __init__(self,noun,symbol,weight,type):
-        self.noun = noun
-        self.symbol = symbol
-        self.weight = weight
-        self.type = type
-    def __repr__(self):
-        return f" {self.noun, self.symbol, self.type}"
